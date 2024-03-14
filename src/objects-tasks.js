e2ca2d5dd553bdee4bfcf596b27fbc7da7cdbaff
Reel <ladias231299@gmail.com>
2024-03-14 21:51:13 +0200

    feat: add solution for sellTickets

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -169,8 +169,17 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let currentMoney = 0;
+  let answer = true;
+  queue.forEach((item) => {
+    if (currentMoney - item >= 0 || item === 25) {
+      currentMoney += item;
+    } else {
+      answer = false;
+    }
+  });
+  return answer;
 }
 
 /**
